7a9146a6f79d1d769fd8cc500c7869f4ebba95e6
srimurooka <murooka.sri@gmail.com>
2012-03-04 16:17:26 -0500

    modify for plots for mongodb

--- routes/join.js
+++ routes/join.js
@@ -11,13 +11,13 @@ exports.index = function(req, res) {
   if (url) {
     Manager.find(url, function(err, amidas) {
       if(!err && amidas.length == 1) {
-                  for (var j = 0; j < amidas[0].items.length; j++ ) {
-                      console.log(amidas[0].items[j].name + "'s position is " + amidas[0].items[j].position);
-                  }
-          res.render('join', {amida: amidas[0]});
+        for (var j = 0; j < amidas[0].items.length; j++ ) {
+          console.log(amidas[0].items[j].name + "'s position is " + amidas[0].items[j].position);
+        }
+        res.render('join', {amida: amidas[0]});
       } else {
-          console.log(err);
-          res.redirect("/");
+        console.log(err);
+        res.redirect("/");
       }
     });
   } else {
@@ -57,7 +57,11 @@ exports.socket = function (app) {
         console.log('connection');
         
         // data
-        client.emit('amida_data', line_data);
+        //client.emit('amida_data', line_data);
+        makeLineData(1, function(data) {
+          client.emit('amida_data', data);
+        });
+        
         
         //クライアント側からmessage受信ハンドラ
         client.on('message', function(message) {
@@ -91,3 +95,28 @@ var line_data = {
                       { start: { x: 0, y: 15 },  end: { x: 5, y: 15 } }
                   ]
 };
+
+var makeLineData = function(url, callback) {
+  if (url) {
+    Manager.find(url, function(err, amidas) {
+      if(!err && amidas.length == 1) {
+        var amida = amidas[0];
+        // 縦棒
+        var items = amida.items;
+        var varticals = [];
+        for (var i = 0 ; i < items.length ; i++) {
+          var x = i * 5;
+          varticals[varticals.length] = { start : { x : x, y : 0 }, end : { x : x, y : 20 }, end_name : items[i].name };
+        }
+        // 横棒
+        var horizontals = amida.plots;
+        console.log("makeLineData");
+        var data = { vertical : varticals, horizontal : horizontals };
+        callback(data);
+      } else {
+        console.log(err);
+        return false;
+      }
+    });
+  }
+}
